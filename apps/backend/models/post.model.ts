1720e4e307ac0f13fa3cf6ba59854790881190eb
Akash5908 <venomkr020@gmail.com>
2026-03-11 16:26:55 +0530

    Initalized mono-repo along with the frontend and backend basic setup

---
+++ apps/backend/models/post.model.ts
@@ -0,0 +1,35 @@
+import { Schema } from "mongoose";
+
+export const Platforms = {
+  Linkedin: "linkedin",
+  X: "x",
+};
+
+export const Status = {
+  Linkedin: "linkedin",
+  X: "x",
+};
+
+const postSchema = new Schema({
+  userId: {
+    type: String,
+    ref: "User",
+    required: true,
+  },
+  baseContent: {
+    type: String,
+    required: true,
+  },
+  platforms: [
+    {
+      platform: Object.values(Platforms),
+      status: Object.values(Status),
+      transformedContent: { type: String, required: true },
+      platformPostId: { type: String },
+      error: { type: String },
+    },
+  ],
+  scheduledAt: { type: Date, required: true },
+  createdAt: { type: Date, required: true },
+  updatedAt: { type: Date, required: true },
+});
